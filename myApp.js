15085690b737309abd503eed3645e6bd62c9b676
duditts <38972513+duditts@users.noreply.github.com>
2025-06-30 12:58:18 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const app = express();
 
 const PORT = process.env.PORT || 3030;
-//app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
 // your code
 
 app.use(
